c3253797cc44d08a55589c7139d6c917c2f03090
COR Students <corstudents@Matts-MBP-5.localdomain>
2018-11-28 10:07:22 -0500

    added gitignore

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(HomePage);
+});
+
+app.get("/help", (req, res) => {
+    res.send("Help Page")
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Me")
+});
+
+app.listen(3000);
